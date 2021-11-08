62fcfe2552ed143968475b450b168116f374bc2c
belhaghassan <51680128+belhaghassan@users.noreply.github.com>
2021-11-07 22:14:53 -0800

    Update piece.py

--- piece.py
+++ piece.py
@@ -1,33 +1,32 @@
 import pygame
 
-from constants import GREY, SQUARE_SIZE
+from constants import GREY, SQUARE_SIZE, RED
+
 class Piece:
-    def __init__(self,row, col, color):
+    PADDING = 7
+    OUTLINE = 2
+
+    def __init__(self, row, col, color):
         self.row = row
         self.col = col
-        self.col = color
+        self.color = color
         self.king = False
-
-        if self.color == RED:
-            self.direction = -1
-        else:
-            self.direction = 1
-
         self.x = 0
         self.y = 0
         self.calc_pos()
 
     def calc_pos(self):
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
-        self. y = SQUARE_SIZE * self.row * SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
     def make_king(self):
         self.king = True
-
+    
     def draw(self, win):
-        radius = SQUARE_SIZE // 2 - 8
+        radius = SQUARE_SIZE//2 - self.PADDING
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
-        pygame.draw.circle(win, GREY, (self.x, self.y), radius + 2)
+
 
     def __repr__(self):
         return str(self.color)
